a290f006572d16ca5bc95eab77a45863d1e4487f
Kjell Seier <kjell-seier@users.noreply.github.com>
2026-05-15 11:28:44 +0000

    refactor(config): centralize env parsing

--- benches/throughput.bench.ts
+++ benches/throughput.bench.ts
@@ -89,3 +89,5 @@ export {};
 // maint: docs: add delegate() example to quickstart (2026-06-16)
 
 // maint: chore(ci): cache node_modules in workflow (2026-06-16)
+
+// maint: refactor(config): centralize env parsing (2026-06-16)
